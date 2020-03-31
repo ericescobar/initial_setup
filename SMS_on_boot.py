9e7cfef2a07338f83dd711c06a84306f4edccdb5
Eric <pi@watcher.localdomain>
2020-03-31 15:32:38 -0700

    Initial Commit

---
+++ SMS_on_boot.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import sendsms
+import time
+import sys
+import requests
+import subprocess
+import netifaces
+
+#######################
+#Check Interfaces
+#######################
+def CheckInterfaces():
+    interfaces = netifaces.interfaces()
+    interface_list = []
+    address_list = []
+    for interface in interfaces:
+
+            if interface != 'lo':
+                    interface_list.append(interface)
+                    try:
+                            addrs = netifaces.ifaddresses(interface)
+                            addrs[netifaces.AF_INET]
+                            #gws=netifaces.gateways()
+                            #print gws['default'][netifaces.AF_INET][0]
+                            address_list.append(addrs[netifaces.AF_INET][0]['addr'])
+                    except:
+                            address_list.append('None')
+                    #print addrs[netifaces.AF_INET][0]['netmask']
+    return address_list, interface_list
+
+######################
+#Check Public IP
+######################
+def CheckPublicIP():
+    #Public URLs that return your public facing IP address
+    publicURLs = ['http://ipecho.net/plain', 'http://ip.42.pl/raw','http://ipinfo.io/ip']
+
+    #Try each URL in the list
+    for URL in publicURLs:
+            #Try each URL until on passes with a 200 http code
+            try:
+                    publicReqIP=requests.get(URL)
+                    if publicReqIP.status_code == 200:
+                            publicIP = publicReqIP.text
+                            #Break out of loop if one comes back
+                            break
+            except:
+                    numURLs= len(publicURLs)
+                    publicIP='Unable to determine external IP from %s URLs internet may be down.' % (numURLs)
+    #Strip out any spaces, newlines, tabs, that stuff
+    publicIP=publicIP.strip(' \t\n\r')
+    return publicIP
+####################
+#Format it all
+####################
+try:
+	publicIP = CheckPublicIP()
+	address_list,interface_list = CheckInterfaces()
+	message = 'Public IP: %s' % (publicIP)
+	i=0
+	total_interfaces = len(interface_list)
+	while i < total_interfaces:
+		message += "\n%s: %s" % (interface_list[i],address_list[i])
+		i=i+1
+	print(message)
+	sendsms.sms(message)
+except:
+    	print("Error message not sent.")
+    	time.sleep(3)
+    	sys.exit(1)
---
+++ sendsms.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+#Importing stuff
+import datetime
+import re
+def sms(sms_body):
+	#Reading from config.txt
+	f = open('config.txt','r')
+	lines = f.read().splitlines()
+	DeviceID = re.findall(r'"([^"]*)"', lines[0])[0]
+	twilio_account = re.findall(r'"([^"]*)"', lines[2])[0]
+	twilio_token = re.findall(r'"([^"]*)"', lines[3])[0]
+	twilio_from = re.findall(r'"([^"]*)"', lines[4])[0]
+	phoneNumbers = re.findall(r'"([^"]*)"', lines[1])[0]
+	phoneNumbers = phoneNumbers.split(",")
+	f.close()
+
+	#Checking & formatting time
+	timedate = datetime.datetime.now()
+	tdate = timedate.strftime('%I:%M%p')
+
+	#Format Message header you want to send
+	sms_message = '%s: %s \n%s' % (DeviceID,tdate,sms_body)
+
+
+	#Sending sms to every number in config.txt
+	for phoneNumber in phoneNumbers:
+		from twilio.rest import Client
+		client = Client(twilio_account, twilio_token)
+		message = client.messages.create(
+               		to=phoneNumber,
+               		from_=twilio_from,
+               		body=sms_message,
+       			)
+
